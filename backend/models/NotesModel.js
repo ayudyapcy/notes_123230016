7f76b31b381d44d22a9edd3919c0005fcd6bbec9
ayudyapcy <ayudyaprameswari22@gmail.com>
2026-05-11 23:26:03 +0700

    update model

--- backend/models/NotesModel.js
+++ backend/models/NotesModel.js
@@ -4,7 +4,7 @@ import db from "../config/database.js";
 const Notes = db.define("notes", {
   judul: Sequelize.STRING,
   isi: Sequelize.TEXT,
-  tanggal: {
+  tanggal_dibuat: {
     type: Sequelize.DATE,
     defaultValue: Sequelize.NOW
   }
